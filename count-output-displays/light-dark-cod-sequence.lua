3c61f1a3b50698bae5ae5be437391f5745af4638
Gordon Woodhull <gordon.woodhull@posit.co>
2025-02-22 13:26:16 -0500

    move caption and crossref experiments to own tree, restore old count-output-displays

--- count-output-displays/light-dark-cod-sequence.lua
+++ count-output-displays/light-dark-cod-sequence.lua
@@ -1,13 +1,12 @@
 function Div(div)
-  -- Only process cell div with light-dark marker
+  -- Only process cell div with ligh-dark marker
   if not div.classes:includes("cell") or not div.classes:includes("quarto-light-dark-container") then
     return nil
   end
-  quarto.log.output('light-dark-cod-sequence')
   local cods = {}
   local indices = {}
   for i, cellOutput in ipairs(div.content) do
-    if cellOutput.classes and cellOutput.classes:includes("cell-output-display") then
+    if cellOutput.classes:includes("cell-output-display") then
       table.insert(cods, cellOutput)
       indices[cellOutput] = i
     end
@@ -25,7 +24,7 @@ function Div(div)
       return nil
     end
   elseif #cods < 2 then
-    quarto.log.warning('not enough cell-output-display to do light/dark', #cods)
+    quarto.log.warning('not enough cell-output-display to do light/dark')
     return nil
   else
     start = 1
@@ -34,13 +33,8 @@ function Div(div)
 
   local lightDiv = cods[start]
   local darkDiv = cods[start+stride]
-
-  quarto.log.output('light', lightDiv)
-  quarto.log.output('dark', darkDiv)
   local nlight = indices[darkDiv] - indices[lightDiv]
   local ndark = #div.content - indices[darkDiv] + 1
-  quarto.log.output('info: #div.content', #div.content, 'nlight', nlight, 'ndark', ndark, 'start', start, 'i[l]', indices[lightDiv], 'stride', stride)
-
   if nlight ~= stride or ndark ~= stride then
     quarto.log.warning('extra non-cell-output-display content detected, nlight', nlight, 'ndark', ndark, 'start', start, 'i[l]', indices[lightDiv], 'stride', stride)
     return nil
@@ -48,64 +42,15 @@ function Div(div)
 
   local lightContents = pandoc.Blocks({})
   local darkContents = pandoc.Blocks({})
-  local caption
-  local identifier = nil
   for i = indices[lightDiv], indices[darkDiv]-1 do
-    local cod = div.content[i] -- light cell-output-display
-    -- remove identifier from figure
-    if cod.content[1].caption and cod.content[1].attr.identifier then
-      identifier = cod.content[1].attr.identifier
-      cod.content[1].attr.identifier = ""
-    -- else remove identifier from image
-    elseif cod.content[1].content
-        and cod.content[1].content[1]
-        and cod.content[1].content[1].caption
-        and cod.content[1].content[1].attr.identifier ~= "" then
-      identifier = cod.content[1].content[1].attr.identifier
-      cod.content[1].content[1].attr.identifier = ""
-    end
-
-    lightContents:insert(cod.content[1])
-    if #cod.content > 1 then
-      caption = cod.content[2]
-    end
+    lightContents:insert(div.content[i].content[1])
   end
-  -- if cell doesn't have identifier, use one from figure/imate, removing number at end
-  if identifier and div.attr.identifier == "" then
-    div.attr.identifier = identifier:gsub('%-%d+$', '')
+  for i = indices[darkDiv], #div.content do
+    darkContents:insert(div.content[i].content[1])
   end
-
-  -- if light is a figure but dark is not, dupe light figure and replace src
-  -- to preserve caption, which only goes on first
-  if stride == 1 and lightDiv.content[1].caption and not darkDiv.content[1].caption then
-    local darkFigure = lightDiv.content[1]:walk {} -- already has identifier removed
-    darkFigure.content[1].content[1].src = darkDiv.content[1].content[1].src
-    darkContents:insert(darkFigure)
-  else
-    for i = indices[darkDiv], #div.content do
-      local cod = div.content[i] -- dark cell-output-display
-      -- remove identifier from figure
-      if cod.content[1].caption then
-        cod.content[1].attr.identifier = ""
-      -- or from image
-      elseif cod.content[1].content
-          and cod.content[1].content[1]
-          and cod.content[1].content[1].caption
-          and cod.content[1].content[1].attr.identifier ~= "" then
-        cod.content[1].content[1].attr.identifier = ""
-      end
-      darkContents:insert(cod.content[1])
-    end
-  end
-
   div.content[indices[lightDiv]] = pandoc.Div(lightContents, pandoc.Attr("", {'quarto-light-content'}, {}))
   div.content[indices[lightDiv] + 1] = pandoc.Div(darkContents, pandoc.Attr("", {'quarto-dark-content'}, {}))
-  local outj = indices[lightDiv] + 1
-  if caption then
-    outj = outj + 1
-    div.content[outj] = caption
-  end
-  for i = outj + 1, #div.content do
+  for i = indices[lightDiv] + 2, #div.content do
     div.content[i] = nil
   end
   return div
