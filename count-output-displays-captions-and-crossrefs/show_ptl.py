f3fd2f187294e3d606ce2b45dd0a417a4da1fce2
Gordon Woodhull <gordon.woodhull@posit.co>
2025-02-27 13:45:57 -0500

    Built site for gh-pages

--- count-output-displays-captions-and-crossrefs/show_ptl.py
+++
@@ -1,4 +0,0 @@
-# print out the plotly layout object to see its parameters
-import plotly.io as pio
-plotly_template = pio.templates["plotly"]
-print(plotly_template.layout)
--- plot-replaying/light-dark-knitr.lua
+++
@@ -1,9 +0,0 @@
-function Image(img)
-  if img.src:match '%.light%.svg' then
-    img2 = img:walk {}
-    img2.src = img.src:gsub('%.light%.svg', '.dark.svg')
-    img.classes:insert 'quarto-light-content'
-    img2.classes:insert 'quarto-dark-content'
-  return pandoc.Inlines({img, img2})
-  end
-end
